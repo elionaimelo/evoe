39a38b652b417f4c25fd836e8396d6f239d751b6
Elionai Melo <elionai.melo@lais.huol.ufrn.br>
2020-02-28 18:48:42 -0300

    inclusão do testemunho, rodape e outras correções

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
-var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true
-    }
-  });
+$('.is-floating-label input, .is-floating-label textarea').on('focus blur', function (e) {
+  $(this).parents('.is-floating-label').toggleClass('is-focused', (e.type === 'focus' || this.value.length > 0));
+}).trigger('blur');
+
+$('#sq-crousal4').carousel({
+  interval: 10000000000 * 10
+});
